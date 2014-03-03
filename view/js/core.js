112cfce98111adbb9008c3f1320e378e8c0609c9
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2014-03-03 10:29:58 +0100

    Added proper editor

--- view/js/core.js
+++ view/js/core.js
@@ -1,3 +1,5 @@
+var editor = undefined;
+
 $(document).ready(function() {
 	/*
 	* Setup view
@@ -10,18 +12,33 @@ $(document).ready(function() {
 
 		PyInterface.log('Saving file to ' + fname);
 
-		PyInterface.save_file(fname, $('#text').val());
+		PyInterface.save_file(fname, editor.getValue());
 	})
 	$('#open')
 	.click(function() {
 		PyInterface.log('Opening file');
 
+		// load file
 		var filename = PyInterface.show_open_file_dialog("Open file");
-		var content = PyInterface.read_file(filename); 
-		$('#text').val(content); 
+		var content = PyInterface.read_file(filename);
+
+		// enable syntax highlighting
+		var modelist = ace.require('ace/ext/modelist');
+		var mode = modelist.getModeForPath(filename).mode;
+		editor.getSession().setMode(mode);
+
+		// set content
+		editor.setValue(content);
+
+		$('#filename').val(filename);
 	}); 
 
-	Events.on("keypress", function(e){
+	// handle events
+	Events.on("keypress", function(e) {
 		// do keypress stuffs here
-	})
+	});
+
+	// enable editor
+	editor = ace.edit("editor");
+	editor.setTheme("ace/theme/monokai");
 });
