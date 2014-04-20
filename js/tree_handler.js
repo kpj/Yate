a588687a745ad29435b5d3ed4957207d002dc0ab
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2014-04-20 10:05:31 +0200

    Refactoring

---
+++ js/tree_handler.js
@@ -0,0 +1,28 @@
+function setupDirTree(dirname) {
+	$('#dir_tree').empty()
+		$('#dir_tree').jstree({
+			'core': {
+				'data': dictToDirTree(dirname)
+			}
+		});
+
+		$('#dir_tree').on("changed.jstree", function (e, data) {
+			// get node name
+			var node = '/' + data.node.text;
+
+			// get its sub-directory
+			var subdir = '';
+			for(var i = data.node.parents.length-1 ; i >= 0 ; i--) {
+				var id = data.node.parents[i];
+
+				if(id != '#') {
+					subdir += '/' + $('#dir_tree').jstree().get_node(id).text;
+				}
+			}
+
+			var filename = dirname + subdir + node;
+
+			if(data.node.data.type == 'file')
+				loadFile(filename);
+		});
+}
